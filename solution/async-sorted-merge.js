151939871dd5fddebfb27919dfd836ef6621993f
winstonxc <winstonxchang@gmail.com>
2024-09-03 20:44:00 +0000

    feat: Add async sync log printers

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,45 @@
 "use strict";
 
+const MinHeap = require("../lib/min-heap");
 // Print all entries, across all of the *async* sources, in chronological order.
+const fetchLogs = async (logSources) => {
+  const promiseArr = [];
 
-module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
+  for(const source of logSources) {
+    if(!source.drained) {
+      promiseArr.push(source.popAsync());
+    }
+  }
+
+  return await Promise.all(promiseArr);
+}
+
+const addLogsToHeap = (logs, minHeap) => {
+  for(const logEntry of logs) {
+    if(logEntry) {
+      minHeap.add({ logEntry });
+    }
+  }
+}
+
+const processHeapLogs = async (minHeap, logSources, printer) => {
+  while (!minHeap.isEmpty()) {
+    const { logEntry } = minHeap.removeMin();
+    printer.print(logEntry);
+
+    let logs = await fetchLogs(logSources);
+    addLogsToHeap(logs, minHeap);
+  }
+}
+
+module.exports = async (logSources, printer) => {
+  const minHeap = new MinHeap();
+  let logs = await fetchLogs(logSources);
+  addLogsToHeap(logs, minHeap);
+
+  return new Promise(async (resolve, reject) => {
+    await processHeapLogs(minHeap, logSources, printer);
+    printer.done();
     resolve(console.log("Async sort complete."));
   });
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,29 @@
 "use strict";
 
-// Print all entries, across all of the sources, in chronological order.
+const MinHeap = require("../lib/min-heap");
 
+// Print all entries, across all of the sources, in chronological order.
 module.exports = (logSources, printer) => {
-  return console.log("Sync sort complete.");
+  const minHeap = new MinHeap();
+
+  for (const source of logSources) {
+    if(!source.drained) {
+      minHeap.add({ hasMoreLogs: !source.drained, logEntry: source.pop(), source });
+    }
+  }
+
+  // Keep processing logs until the heap is empty and all the logs are drained
+  while (!minHeap.isEmpty()) {
+    const { logEntry, source, hasMoreLogs } = minHeap.removeMin();
+    printer.print(logEntry);
+
+    if (hasMoreLogs) {
+      const nextLogEntry = source.pop();
+      if(nextLogEntry) {
+        minHeap.add({ hasMoreLogs: !source.drained, logEntry: nextLogEntry, source });
+      }
+    }
+  }
+
+  printer.done();
 };
